982a2ed8a5ee0681805d0b0cbbf7b77bcacba2aa
HimbeerserverDE <himbeerserverde@gmail.com>
2022-04-24 18:17:36 +0200

    Add tests

---
+++ test/test_strcon.c
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include "../strcon.h"
+
+int main()
+{
+	printf("--------------\n");
+	printf("testing strcon\n");
+	printf("--------------\n");
+
+	char strcon_dest[11] = "foo";
+	char *strcon_result = strcon(strcon_dest, "bar", "test", NULL);
+
+	printf("testing strcon_result: exp: %p, got: %p\n", strcon_dest, strcon_result);
+	assert(strcon_result == strcon_dest);
+
+	printf("testing strcon_dest: exp: foobartest, got: %s\n", strcon_dest);
+	assert(strcmp(strcon_dest, "foobartest") == 0);
+
+	printf("---------------\n");
+	printf("testing astrcon\n");
+	printf("---------------\n");
+
+	char *astrcon_result = astrcon("foo", "bar", "test", NULL);
+
+	printf("testing astrcon_result: exp: foobartest, got: %s\n", astrcon_result);
+	assert(strcmp(astrcon_result, "foobartest") == 0);
+}
